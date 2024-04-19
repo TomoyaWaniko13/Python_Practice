6f781211d9325290a2b7fc366185a9c17feb28b2
TomoyaWaniko <alligatorfree12@gmail.com>
2024-04-19 22:11:39 +0900

    Update

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,1 +1,52 @@
-print('hello!')
+# print('hello!')
+#
+
+# name = input('What is your name?')
+# print(name)
+
+# print(type(2))
+# print(type(False))
+# print(type('Kevin'))
+
+# print(9.9 + 0.1)
+# print(5 // 4)
+# print(6 % 4)
+
+# items = ['notebooks', 'sunglasses', 'toys', 'grapes']
+# print(items[0:1])
+# print(items[0:3])
+#
+# print(items[0::1])
+# print(items[0::2])
+
+# items = ['notebooks', 'sunglasses', 'toys', 'grapes']
+# sameItems = items
+# sameItems[0] = 'apple'
+# print(items)
+# copiedItems = items[0:4]
+# copiedItems[0] = 'apple'
+# print(items)
+
+# matrix = [
+#     [1, 5, 1],
+#     [0, 1, 0],
+#     [1, 0, 1],
+# ]
+#
+# print(matrix[0][2])
+
+# tuple1 = (1, 2, 3, 4, 5)
+# TypeError: 'tuple' object does not support item assignment
+# tuple1[0] = 2
+
+tuple1 = (1, 2, 3, 4, 5)
+print(5 in tuple1)
+user = {
+    'basket': [1,2,3],
+    'greet': 'hello',
+    'age': 20
+}
+
+print(user.items())
+
+
